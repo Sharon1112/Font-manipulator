befe8cf426cab059c83ac89b0a8aaeb38dda9659
sc <84851220+Sharon1112@users.noreply.github.com>
2023-01-11 16:46:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-function setup(){
-    video = createCapture(VIDEOO);
-    video.size(550, 500);
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas = createCanvas(550, 500);
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(350, 350);
+
+    canvas = createCanvas(1000, 650);
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -16,5 +22,19 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+       
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
     }
+}
+
+function draw() {
+    background('#000000');
+    
+    text('It is not what the world holds for you but instead it is what you bring to it. :) ', 50, 350);
+    textSize(difference);
+    fill("white");
 }
